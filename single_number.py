0effcb98f820e78fdf860748346c78f95fb4ec75
VS <devStarkes@gmail.com>
2019-10-12 16:55:07 +0300

    Create single_number.py

---
+++ single_number.py
@@ -0,0 +1,25 @@
+def single_umber(nums):
+    hash_table = {}
+
+    for i in nums:
+        try:
+            hash_table.pop(i)
+        except:
+            hash_table[i] = 1
+
+    return hash_table.popitem()[0]
+
+
+def single_umber_on_steroids(nums):
+    a = 0
+
+    for i in nums:
+        a ^= i
+
+    return a
+
+
+if __name__ == "__main__":
+    nums = [2, 3, 2, 4, 3, 4, 0, 8, 2, 7, 0, 8, 7, 1, 2]
+    single_umber(nums)
+    single_umber2(nums)
