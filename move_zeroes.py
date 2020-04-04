adf911bab7f172268d79db8cdb8c49bc81aa975c
VS <devStarkes@gmail.com>
2020-04-04 21:04:52 +0300

    Update move_zeroes.py

--- move_zeroes.py
+++ move_zeroes.py
@@ -8,3 +8,10 @@ class Solution:
         
         for i in range(non_zero_index, len(nums)):
             nums[i] = 0
+    
+    def moveZeroes2(self, nums: List[int]) -> None:
+        non_zero_index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[non_zero_index] = nums[non_zero_index], nums[i]
+                non_zero_index += 1
